79ac0797eaa9a6e1acf16ee4e4fc43fd8531c998
VitorSchuler <vschulervellosoborges@gmail.com>
2025-12-17 16:01:59 -0300

    authService - tecnico

--- src/main/java/com/ufcg/psoft/commerce/service/auth/AuthService.java
+++ src/main/java/com/ufcg/psoft/commerce/service/auth/AuthService.java
@@ -2,4 +2,5 @@ package com.ufcg.psoft.commerce.service.auth;
 
 public interface AuthService {
     void autenticarCliente(Long id, String codigoAcesso);
+    void autenticarTecnico(Long id, String codigoAcesso);
 }
--- src/main/java/com/ufcg/psoft/commerce/service/auth/AuthServiceImpl.java
+++ src/main/java/com/ufcg/psoft/commerce/service/auth/AuthServiceImpl.java
@@ -1,24 +1,38 @@
 package com.ufcg.psoft.commerce.service.auth;
 
 import com.ufcg.psoft.commerce.exception.ClienteNaoExisteException;
+import com.ufcg.psoft.commerce.exception.TecnicoNaoExisteException;
 import com.ufcg.psoft.commerce.exception.CodigoDeAcessoInvalidoException;
 import com.ufcg.psoft.commerce.model.Cliente;
+import com.ufcg.psoft.commerce.model.Tecnico;
 import com.ufcg.psoft.commerce.repository.ClienteRepository;
+import com.ufcg.psoft.commerce.repository.TecnicoRepository;
 import org.springframework.stereotype.Service;
 
 
 @Service
 public class AuthServiceImpl implements AuthService{
     private final ClienteRepository clienteRepository;
+    private final TecnicoRepository tecnicoRepository;
 
-    public AuthServiceImpl(ClienteRepository c){
+    public AuthServiceImpl(ClienteRepository c, TecnicoRepository t){
         this.clienteRepository = c;
+        this.tecnicoRepository = t;
+
     }
 
     @Override
     public void autenticarCliente(Long id, String codigoAcesso) {
         Cliente cliente = clienteRepository.findById(id).orElseThrow(ClienteNaoExisteException::new);
         if (!cliente.getCodigo().equals(codigoAcesso)) {
+            throw new CodigoDeAcessoInvalidoException();
+        }
+    }
+
+    @Override
+    public void autenticarTecnico(Long id, String codigoAcesso) {
+        Tecnico tecnico = tecnicoRepository.findById(id).orElseThrow(TecnicoNaoExisteException::new);
+        if (!tecnico.getCodigoAcesso().equals(codigoAcesso)) { 
             throw new CodigoDeAcessoInvalidoException();
         }
     }
